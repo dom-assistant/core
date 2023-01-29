fd22016d6d9f66a03a21fea1b8c888c8c96ef05a
dom-assistant <9159485@mail.ru>
2023-01-29 16:55:36 +0300

    Update config.js

--- front/src/config.js
+++ front/src/config.js
@@ -1,5 +1,5 @@
 const local = {
-  gladysGatewayApiUrl: process.env.GLADYS_GATEWAY_API_URL || 'https://api.gladysgateway.com',
+  gladysGatewayApiUrl: process.env.GLADYS_GATEWAY_API_URL || 'http://localhost:3000',
   localApiUrl: process.env.LOCAL_API_URL || 'http://localhost:1443',
   webSocketUrl: process.env.WEBSOCKET_URL || 'ws://localhost:1443',
   gatewayMode: process.env.GATEWAY_MODE === 'true',
